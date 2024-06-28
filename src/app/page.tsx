b1a2e06ce56a02bcff35b8728ab856062953e944
Matheus <matheuschmidt.b@gmail.com>
2024-06-28 10:43:45 -0300

    feat: add first promo banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/categoryList";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -14,6 +15,15 @@ export default function Home() {
       <div>
         <CategoryList />
       </div>
+
+      <Image
+        src="/promo_1.png"
+        alt="Até 30% de descontos em pizzas"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto w-full object-contain px-5 pt-6"
+      />
     </main>
   );
 }
